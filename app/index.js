3aab5dbda2686e0c14a402914f1dd1c20a5226f2
Fer <fer.falcon.dg@gmail.com>
2025-11-15 12:09:49 -0300

    Parsing request paths

--- app/index.js
+++ app/index.js
@@ -1,7 +1,16 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req,res) => {
-  res.end('Hello World\n');
+
+  const parseUrl = url.parse(req.url, true);
+  // console.log(parseUrl);
+  const path = parseUrl.pathname;
+  // console.log(path);
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  console.log(`Request received on path: ${trimmedPath}`);
+
+  res.end('Hi there!\n');
 });
 
 server.listen(3000, () => {
