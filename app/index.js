bd898afde35508ce6c98715420f8d4c038feb030
Fer <fer.falcon.dg@gmail.com>
2025-11-15 14:01:42 -0300

    Parsing Payloads

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 const server = http.createServer((req,res) => {
 
@@ -14,21 +15,33 @@ const server = http.createServer((req,res) => {
 
   const headers = req.headers;
 
-  console.log(
-    `Request received:\n` +
-    `path: '${trimmedPath}'\n` +
-    `method: '${method}'`
-  );
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', data => {
+    buffer += decoder.write(data);
+  });
+  req.on('end', () => {
+    buffer += decoder.end();
+  
+    console.log(
+      `Request received:\n` +
+      `path: '${trimmedPath}'\n` +
+      `method: '${method}'`
+    );
+  
+    if (Object.keys(queryStringObject).length) {
+      console.log(`Query string parameters:`);
+      Object.keys(queryStringObject).forEach(key => console.log(`${key}: ${queryStringObject[key]}`));
+    }
+  
+    if (Object.keys(headers).length) {
+      console.log(`Headers:`);
+      Object.keys(headers).forEach(key => console.log(`${key}: ${headers[key]}`));
+    }
 
-  if (Object.keys(queryStringObject).length) {
-    console.log(`Query string parameters:`);
-    Object.keys(queryStringObject).forEach(key => console.log(`${key}: ${queryStringObject[key]}`));
-  }
-
-  if (Object.keys(headers).length) {
-    console.log(`Headers:`);
-    Object.keys(headers).forEach(key => console.log(`${key}: ${headers[key]}`))
-  }
+    console.log(buffer);
+  })
+  
 
   res.end('Hi there!\n');
 });
